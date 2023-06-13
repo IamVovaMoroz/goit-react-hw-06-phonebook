338c14b209d1ab7cd39cc1128fb0161854982046
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-06-13 17:31:17 +0200

    ok

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,3 +1,60 @@
+// // Redux Toolkit 
+// import { configureStore } from "@reduxjs/toolkit";
+// // для работы с localStorage
+
+import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+import { reducer  } from "./reducer";
+
+// persistConfig  добавляем в новый редюсер persistedReducer
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+const persistedReducer = persistReducer(persistConfig, reducer)
+// export const store = configureStore({reducer});
+export const store = configureStore({reducer: persistedReducer});
+
+export const persistor = persistStore(store)
+
+
+// import { reducer } from "./reducer";
+// // import { reducer  } from "./reducer";
+// // 1 если создавали как rootReducer, запись ниже
+// // import { reducer as rootReducer } from "./reducer";
+// // export const store = configureStore({reducer: rootReducer});
+
+// // 2 если rootReducer назван reducer , то
+
+
+
+
+ 
+// const persistedReducer = persistReducer(persistConfig, reducer)
+
+// export const store = configureStore({reducer: persistedReducer});
+// // reducer становится ключём в configureStore, а persistedReducer  значением
+// // export const store = configureStore({reducer}); это было, выше то что стало
+
+// export const persistor = persistStore(store)
+// после этого в оборачиваем index.js   <PersistGate loading={null} persistor={persistor}>
+    // <App />
+    // </PersistGate>
+
+// 1 если создавали как rootReducer, запись ниже
+// import { reducer as rootReducer } from "./reducer";
+// export const store = configureStore({reducer: rootReducer});
+
+// 2 если rootReducer назван reducer , то
+
+// import { reducer  } from "./reducer";
+// export const store = configureStore({reducer});
+
+
+
+
 // import {  configureStore } from '@reduxjs/toolkit';
 // // import { tasksReducer, filtersReducer } from "./reducer";
 // // import { tasksReducer } from "./tasksSlice";
@@ -26,14 +83,9 @@
 // export const store = createStore(reducer);
 
 
-
-// Redux Toolkit 
-import { configureStore } from "@reduxjs/toolkit";
 // 1 если создавали как rootReducer, запись ниже
 // import { reducer as rootReducer } from "./reducer";
 // export const store = configureStore({reducer: rootReducer});
 
 // 2 если rootReducer назван reducer , то
 
-import { reducer  } from "./reducer";
-export const store = configureStore({reducer});
